6293dfa678b7aa7ef7b63150b072086c025c038c
seesee010 <sebastian.daubner2@gmail.com>
2026-04-27 12:33:17 +0200

    FIX(getLine - win version): win version of getline is now fixed!

---
+++ src/lo3-getLine.c
@@ -0,0 +1,49 @@
+#include "./internal/core.h"
+#ifdef _WIN32
+
+typedef long long ssize_t;
+ssize_t lo3_getLine(char **lineptr, size_t *n, FILE *stream) {
+
+	if (lineptr == NULL || n == NULL || stream == NULL) {
+		return -1;
+	}
+
+	if (*lineptr == NULL || *n == 0) {
+
+		*n = 128;
+		*lineptr = malloc(*n);
+		if (*lineptr == NULL) {
+			return -1;
+		}
+	}
+
+	int pos = 0;
+	int c;
+	while ((c = fgetc(stream)) != EOF) {
+		
+		if (pos + 1 >= *n) {
+
+			size_t new_size = *n * 2;
+			char *tmp = realloc(*lineptr, new_size);
+			if (tmp == NULL) {
+				return -1;
+			}
+			
+			*lineptr = tmp;
+			*n = new_size;
+		}
+
+		(*lineptr)[pos++] = (char)c;
+		if (c == '\n') {
+			break;
+		}
+	}
+
+	if (pos == 0) {
+		return -1;
+	}
+
+	(*lineptr)[pos] = '\0';
+	return (ssize_t)pos;
+}
+#endif
---
+++ test/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/sebas/Desktop/lo3-core/test
+# Build directory: C:/Users/sebas/Desktop/lo3-core/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_parsing "C:/Users/sebas/Desktop/lo3-core/bin/test_parsing.exe")
+set_tests_properties(test_parsing PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/sebas/Desktop/lo3-core/test/CMakeLists.txt;34;add_test;C:/Users/sebas/Desktop/lo3-core/test/CMakeLists.txt;38;add_lo3_test;C:/Users/sebas/Desktop/lo3-core/test/CMakeLists.txt;0;")
+add_test(test_execute "C:/Users/sebas/Desktop/lo3-core/bin/test_execute.exe")
+set_tests_properties(test_execute PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/sebas/Desktop/lo3-core/test/CMakeLists.txt;34;add_test;C:/Users/sebas/Desktop/lo3-core/test/CMakeLists.txt;41;add_lo3_test;C:/Users/sebas/Desktop/lo3-core/test/CMakeLists.txt;0;")
