98e5d1e65b15efe43feb8922a8f8da826ac206f8
0xRobert <48176914+zahanzo@users.noreply.github.com>
2026-06-13 16:42:55 -0300

    style: fix indentation, apply review changes for nesting and inline code
    - Extract tokenization logic into a static helper function "pars_tokenize" to keep nesting intensity strictly below 4 layers.
    - Fix inline block styling by adding proper line breaks and braces to single-line "if" statements.
    - Format "else if" statements to start on a new line per repository convention.
    - Clean up temporary debug comments and update the format string to "printf("%s\n", name)" in "src/execute.c".
    - Ensure all modified blocks strictly use tabs for indentation instead of spaces.
    - Update "stress_test.lo3" syntax to use the native "$3" string type identifier.

--- src/parsing.c
+++ src/parsing.c
@@ -1,4 +1,5 @@
 // Copyright (c) 2026 seesee010
+// Copyright (c) 2026 zahanzo
 // Read the License file for more informations about the license.
 //
 // You can find todos by reading the "todo" blocks.
@@ -36,12 +37,50 @@ int pars_isFileValid(char *name, FILE **file) {
 	return 0;
 }
 
-int pars_file(FILE *file) {
-    char *line = NULL;
-    size_t len = 0;
-    char arg1[64] = {0}, arg2[64] = {0};
+// 0xRobert: Function needed to not break intensity
+static char* pars_tokenize(char *ptr, char *arg) {
+	int j = 0;
 
-    int pos0 = ftell(file);
+	while (*ptr == ' ' || *ptr == '\t') {
+		ptr++;
+	}
+
+	if (*ptr == '\0') {
+		return NULL;
+	}
+
+	if (*ptr == '"') {
+		arg[j++] = TYPE_string;
+		ptr++;
+
+		while (*ptr && j < 62) {
+			if (*ptr == '\\' && *(ptr + 1) != '\0') {
+				arg[j++] = *ptr++;
+				arg[j++] = *ptr++;
+				continue;
+			}
+			if (*ptr == '"') {
+				ptr++;
+				break;
+			}
+			arg[j++] = *ptr++;
+		}
+		arg[j] = '\0';
+	}else{
+		while (*ptr && *ptr != ' ' && *ptr != '\t' && j < 62) {
+			arg[j++] = *ptr++;
+		}
+		arg[j] = '\0';
+	}
+	return ptr;
+}
+
+int pars_file(FILE *file) {
+	char *line = NULL;
+	size_t len = 0;
+	char arg1[64] = {0}, arg2[64] = {0};
+
+	int pos0 = ftell(file);
 
 	// todo: make getline avaible on other os
 	//
@@ -52,123 +91,103 @@ int pars_file(FILE *file) {
 	// - use the preprocessor funcs, like ifdef check for linux and win etc
 
 parsing:
-    while (lastLineOffset = ftell(file), GETLINE(&line, &len, file)) {
+	while(lastLineOffset = ftell(file), GETLINE(&line, &len, file)) {
 
-        currentLine++;
-        line[strcspn(line, "\r\n")] = '\0'; 
+		currentLine++;
+		line[strcspn(line, "\r\n")] = '\0'; 
 
-        // 0xRobert: Ignore emoty lines or comments
-        if (line[0] == '\0' || (line[0] == '/' && line[1] == '/')) {
-            free(line);
-            line = NULL;
-            len = 0;
-            continue;
-        }
+		// 0xRobert: Ignore emoty lines or comments
+		if (line[0] == '\0' || (line[0] == '/' && line[1] == '/')) {
+			free(line);
+			line = NULL;
+			len = 0;
+			continue;
+		}
 
-        // 0xRobert: Treats syntax sugar
-        if (line[0] == '@') {
-            if (line[1] == '.') {
-                int idx = 2;
-                while (line[idx] == ' ' || line[idx] == '\t') idx++;
-                if (line[idx] != '\0') {
-                    LO3_STARTING_LINE = line[idx];
-                }
+		// 0xRobert: Treats syntax sugar
+		if (line[0] == '@') {
+			if (line[1] == '.') {
+				int idx = 2;
+				while (line[idx] == ' ' || line[idx] == '\t') idx++;
+				if (line[idx] != '\0') {
+					LO3_STARTING_LINE = line[idx];
+				}
 
-                free(line);
-                line = NULL;
-                len = 0;
-                continue; 
-            } else if (line[1] == '{') {
-                g_fasterInit(line);
-                free(line);
-                line = NULL;
-                len = 0;
-                continue;
-            }
-        }
+				free(line);
+				line = NULL;
+				len = 0;
+				continue; 
+			}
+			else if (line[1] == '{') {
+				g_fasterInit(line);
+				free(line);
+				line = NULL;
+				len = 0;
+				continue;
+			}
+		}
 
-        // 0xRobert: Verify if the line starts with a character invalid of cmd
-        if (line[0] != LO3_STARTING_LINE) {
-            free(line);
-            line = NULL;
-            len = 0;
-            continue;
-        }
+		// 0xRobert: Verify if the line starts with a character invalid of cmd
+		if (line[0] != LO3_STARTING_LINE) {
+			free(line);
+			line = NULL;
+			len = 0;
+			continue;
+		}
 
-        if (strlen(line) < 3) {
-            lo3_warn("You used some kind of magic line...", "");
-            free(line);
-            line = NULL;
-            len = 0;
-            continue;
-        }
+		if (strlen(line) < 3) {
+			lo3_warn("You used some kind of magic line...", "");
+			free(line);
+			line = NULL;
+			len = 0;
+			continue;
+		}
 
-        lo3_cmds cmds = (lo3_cmds)line[1];
+		lo3_cmds cmds = (lo3_cmds)line[1];
 
-        memset(arg1, 0, sizeof(arg1));
-        memset(arg2, 0, sizeof(arg2));
+		memset(arg1, 0, sizeof(arg1));
+		memset(arg2, 0, sizeof(arg2));
 
-        char *ptr = &line[3];
-        int arg_idx = 0;
-        char *args[2] = {arg1, arg2};
+		char *ptr = &line[3];
+		int arg_idx = 0;
+		char *args[2] = {arg1, arg2};
 
-        while (arg_idx < 2) {
-            while (*ptr == ' ' || *ptr == '\t') ptr++; 
-            if (*ptr == '\0') break; 
+	   // 0xRobert: Simplify loop to not break intensity
+		while(arg_idx < 2) {
+			ptr = pars_tokenize(ptr, args[arg_idx]);
+			
+			if (ptr == NULL) {
+				break;
+			}
+			arg_idx++;
+		}
 
-            int j = 0;
-            if (*ptr == '"') {
-                args[arg_idx][j++] = TYPE_string; 
-                ptr++; 
-                
-                while (*ptr && j < 62) {
-                    if (*ptr == '\\' && *(ptr + 1) != '\0') {
-                        args[arg_idx][j++] = *ptr++;
-                        args[arg_idx][j++] = *ptr++;
-                        continue;
-                    }
-                    if (*ptr == '"') {
-                        ptr++; 
-                        break;
-                    }
-                    args[arg_idx][j++] = *ptr++;
-                }
-                args[arg_idx][j] = '\0';
-            } else {
-                while (*ptr && *ptr != ' ' && *ptr != '\t' && j < 62) {
-                    args[arg_idx][j++] = *ptr++;
-                }
-                args[arg_idx][j] = '\0';
-            }
-            arg_idx++;
-        }
+		lo3_val a1 = pars_resv(arg1);
+		lo3_val a2 = pars_resv(arg2);
 
-        lo3_val a1 = pars_resv(arg1);
-        lo3_val a2 = pars_resv(arg2);
+		signed int returnVal = pars_dispatch(cmds, a1, a2);
 
-        signed int returnVal = pars_dispatch(cmds, a1, a2);
+		if(returnVal != -1) {
+			free(line);
+			return returnVal;
+		}
 
-        if (returnVal != -1) {
-            free(line);
-            return returnVal;
-        }
+		free(line);
+		line = NULL;
+		len = 0;
+	}
 
-        free(line);
-        line = NULL;
-        len = 0;
-    }
-
-    if (rush) {
-        fseek(file, pos0, SEEK_SET);
-        if (!isWarped) {
-            isWarped = TRUE;
-            goto parsing;
-        } else {
-            lo3_error("Could not find the label...", "");
-            return -1;
-        }
-    }
-    return 0;
+	if(rush) {
+		fseek(file, pos0, SEEK_SET);
+		if (!isWarped) {
+			isWarped = TRUE;
+			goto parsing;
+		}else{
+			lo3_error("Could not find the label...", "");
+			return -1;
+		}
+	}
+	return 0;
 }
 
 lo3_val pars_resv(char type[64]) {
@@ -263,10 +282,10 @@ lo3_val pars_resv(char type[64]) {
 	default:
 
 		lo3_error("Could not fild the corresponding type! …\n Please enter "
-		          "something valid,"
-		          "You may want to visit "
-		          "https://github.com/lo3-lang/learn-the-syntax !!!",
-		          type);
+				  "something valid,"
+				  "You may want to visit "
+				  "https://github.com/lo3-lang/learn-the-syntax !!!",
+				  type);
 		break;
 	}
 	return result;
